9c51f3e992926e23bb4b2667fe4e344e62e190ac
Anurag <tlannister745@gmail.com>
2024-04-12 23:27:39 +0530

    A

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "./NavbarStyles.css"
+import { Link } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa"
+import { useState } from 'react'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    const [click, setClick] = useState(false)
+    const [color, setColor] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    const handleColor = () => {
+        if (window.scrollY >= 100) {
+            setColor(true)
+        } else {
+            setColor(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleColor)
+
+        return () => {
+            window.removeEventListener('scroll', handleColor)
+        }
+    }, [])
+
+    return (
+        <div className={color ? "header header-bg" : "header"} >
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+            <ul className={click ? ('nav-menu active') : ('nav-menu')}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (
+                    <FaTimes size={25} style={{ color: "white" }} />) : (
+                    <FaBars size={25} style={{ color: "white" }} />)}
+            </div>
+        </div>
+    )
 }
 
 export default NavBar

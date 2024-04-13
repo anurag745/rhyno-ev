91cb6a45df99c12aaa2643c0d2d7d1eca96b570c
Anurag <tlannister745@gmail.com>
2024-04-14 00:52:21 +0530

    Footer

---
+++ src/components/HeroSec.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HeroSecStyle.css'
+const HeroSec = () => {
+  return (
+    <div>HeroSec</div>
+  )
+}
+
+export default HeroSec
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import HeroSec from '../components/HeroSec'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
+    <>
     <NavBar/>
+    <HeroSec/>
+    <Footer/>
+    </>
   )
 }
 
